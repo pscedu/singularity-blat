0307cb63685f337be3e113f4821bf28e1eecd822
Ivan Cao-Berg <icaoberg@psc.edu>
2021-01-05 15:53:43 -0500

    :bug: Removed warning
    Removed annoying warning message and updated scripts

--- 36/pull.sh
+++ 36/pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v singularity)" ]; then
-	module load singularity/3.5.3
+	module load singularity
 fi
 
 singularity pull https://depot.galaxyproject.org/singularity/blat%3A36--0
