d9d19803db91b6bc93b48695123e2777bf6b8790
Ivan Cao-Berg <icaoberg@psc.edu>
2021-01-07 14:58:28 -0500

    :bug: Removed wrong folder
    Somehow I added BLAT 36 to this repo

--- 36/pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! [ -x "$(command -v singularity)" ]; then
-	module load singularity
-fi
-
-singularity pull https://depot.galaxyproject.org/singularity/blat%3A36--0
-mv blat%3A36--0 singularity-blat-36.sif
